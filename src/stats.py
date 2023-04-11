6f99eb59cde97ce0689b0b0663bae58fab91bea4
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-04-11 03:31:47 -0400

    Minor Refactor

--- src/stats.py
+++ src/stats.py
@@ -1,54 +1,60 @@
 import math
 import random
+
+from data.col import Num
 from options import options
-from num import Num
 from utils import many
 
 random.seed(0)
 
-def RX(t,s) :
+
+def RX(t, s):
     t = sorted(t)
-    return {"name":s or "", 
-            "rank":0, 
-            "n":len(t), 
-            "show":"", 
-            "has":t} 
+    return {"name": s or "",
+            "rank": 0,
+            "n": len(t),
+            "show": "",
+            "has": t}
+
 
 def mid(t):
-    t= t.get("has", t)
-    n = len(t)//2
-    return (t[n] +t[n+1])/2 if len(t)%2==0 else t[n+1]
+    t = t.get("has", t)
+    n = len(t) // 2
+    return (t[n] + t[n + 1]) / 2 if len(t) % 2 == 0 else t[n + 1]
+
 
 def div(t):
-    t= t.get("has", t)
-    return (t[ len(t)*9//10 ] - t[ len(t)*1//10 ])/2.56
+    t = t.get("has", t)
+    return (t[len(t) * 9 // 10] - t[len(t) * 1 // 10]) / 2.56
 
 
 def delta(i, other):
     e, y, z = 1E-32, i, other
     return abs(y.mu - z.mu) / ((e + y.sd ** 2 / y.n + z.sd ** 2 / z.n) ** .5)
 
-def merge(rx1,rx2) :
+
+def merge(rx1, rx2):
     rx3 = RX([], rx1['name'])
-    for _,t in enumerate([rx1['has'],rx2['has']]):
-        for _,x in enumerate(t): 
+    for _, t in enumerate([rx1['has'], rx2['has']]):
+        for _, x in enumerate(t):
             rx3['has'].append(x)
     rx3['has'] = sorted(rx3['has'])
     rx3['n'] = len(rx3['has'])
     return rx3
 
-def samples(t,n=0):
-    u= []
+
+def samples(t, n=0):
+    u = []
     n = n or len(t)
-    for i in range(n): 
-        u.append(t[random.randrange(len(t))]) 
+    for i in range(n):
+        u.append(t[random.randrange(len(t))])
     return u
 
-def gaussian(mu,sd): #  #--> n; return a sample from a Gaussian with mean `mu` and sd `sd`
-    mu,sd = mu or 0, sd or 1
-    sq,pi,log,cos,r = math.sqrt,math.pi,math.log,math.cos,random.random
-    return  mu + sd * sq(-2*log(r())) * cos(2*pi*r())
 
+def gaussian(mu, sd):  # #--> n; return a sample from a Gaussian with mean `mu` and sd `sd`
+    mu, sd = mu or 0, sd or 1
+    sq, pi, log, cos, r = math.sqrt, math.pi, math.log, math.cos, random.random
+    return mu + sd * sq(-2 * log(r())) * cos(2 * pi * r())
 
 
 def bootstrap(y0, z0):
@@ -80,6 +86,7 @@ def bootstrap(y0, z0):
 
     return n / options["Bootstrap"] >= options["Conf"]
 
+
 def cliffsDelta(ns1, ns2):
     if len(ns1) > 256:
         ns1 = many(ns1, 256)
@@ -101,4 +108,3 @@ def cliffsDelta(ns1, ns2):
                 lt = lt + 1
 
     return abs(lt - gt) / n <= options["cliff"]
-
