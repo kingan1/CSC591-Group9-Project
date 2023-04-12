a59eccf2c3ca8857660ed74d5ecd576d376d7352
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-04-11 23:29:30 -0400

    Removed unused code

--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 from data.col import Num
@@ -6,25 +5,6 @@ from options import options
 from utils import many
 
 random.seed(0)
-
-
-def RX(t, s):
-    t = sorted(t)
-
-    return {
-        "name": s or "",
-        "rank": 0,
-        "n": len(t),
-        "show": "",
-        "has": t
-    }
-
-
-def mid(t):
-    t = t.get("has", t)
-    n = len(t) // 2
-
-    return (t[n] + t[n + 1]) / 2 if len(t) % 2 == 0 else t[n + 1]
 
 
 def div(t):
@@ -38,19 +18,6 @@ def delta(i, other):
     return abs(y.mu - z.mu) / ((e + y.sd ** 2 / y.n + z.sd ** 2 / z.n) ** .5)
 
 
-def merge(rx1, rx2):
-    rx3 = RX([], rx1['name'])
-
-    for _, t in enumerate([rx1['has'], rx2['has']]):
-        for _, x in enumerate(t):
-            rx3['has'].append(x)
-
-    rx3['has'] = sorted(rx3['has'])
-    rx3['n'] = len(rx3['has'])
-
-    return rx3
-
-
 def samples(t, n=0):
     u = []
     n = n or len(t)
@@ -59,13 +26,6 @@ def samples(t, n=0):
         u.append(t[random.randrange(len(t))])
 
     return u
-
-
-def gaussian(mu, sd):  # #--> n; return a sample from a Gaussian with mean `mu` and sd `sd`
-    mu, sd = mu or 0, sd or 1
-    sq, pi, log, cos, r = math.sqrt, math.pi, math.log, math.cos, random.random
-
-    return mu + sd * sq(-2 * log(r())) * cos(2 * pi * r())
 
 
 def bootstrap(y0, z0):
