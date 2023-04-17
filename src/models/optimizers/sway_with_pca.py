f99c8100c3bf01ec067178c967815e76d5f5cd9f
j-08-shah <08jainam05shah@gmail.com>
2023-04-16 20:41:50 -0400

    agglo

--- src/models/optimizers/sway_with_pca.py
+++ src/models/optimizers/sway_with_pca.py
@@ -1,19 +1,20 @@
-from typing import List, Optional
-
-from sklearn.decomposition import FactorAnalysis
-
+from typing import Optional, List
+import random
+import math
+import numpy as np
 from data import Data
-from data.col import Col, Num, Sym
-from distance import cosine_similarity, PDist, Distance
+from data.col import Col
+from data.row import Row
+from distance import Distance, PDist, cosine_similarity
 from predicate import ZitzlerPredicate
 from utils import many, any
-
+from .base import BaseOptimizer
+from sklearn.cluster import AgglomerativeClustering
 
 class SwayWithFAOptimizer:
     def __init__(self, distance_class: Distance = None, reuse: bool = True, far: float = 0.95, halves: int = 512,
                  rest: int = 10, i_min: float = 0.5):
         self._data: Optional[Data] = None
-        self._fa_rows: List[List[float]] = [[]]
 
         self._distance_class = distance_class or PDist(p=2)
 
@@ -25,93 +26,113 @@ class SwayWithFAOptimizer:
 
     def run(self, data: Data):
         self._data: Data = data
-        self._run_fa(self._data.cols.x)
 
-        return self._sway()
+        return self.sway_agglo(self._data.cols.x)
+    
+    def half_agglo(self, rows=None, cols=None):
+        if not rows:
+            rows = self._data.rowsrows
 
-    def _run_fa(self, cols: List[Col]):
-        input_ = [
-            [
-                col.normalize(row.cells[col.at]) if row.cells[col.at] != "?" else col.normalize(col.mid())
-                for col in cols if isinstance(col, Num)
-            ]
-            for row in self._data.rows
-        ]
+        rows_numpy = np.array([r.cells for r in rows])
+        agg = AgglomerativeClustering(n_clusters=2, metric='euclidean', linkage='ward').fit(rows_numpy)
+        left = []
+        right = []
+        for idx, label in enumerate(agg.labels_):
+            if label == 0:
+                left.append(rows[idx])
+            else:
+                right.append(rows[idx])
+        return left, right, random.choices(left, k=10), random.choices(right, k=10)
+    
+    def sway_agglo(self, cols=None):
+        
+        def worker(rows, worse):
+            if len(rows) <= len(self._data.rows) ** self._i_min:
+                return rows, many(worse, self._rest * len(rows)),
+            l, r, A, B = self.half_agglo(rows, cols)
+            if self.better_multiple(B, A):
+                l, r, A, B = r, l, B, A
+            for x in r:
+                worse.append(x)
+            return worker(l, worse)
 
-        for i in range(len(self._data.rows)):
-            for col in cols:
-                if isinstance(col, Sym):
-                    value = self._data.rows[i].cells[col.at]
-
-                    input_[i] += [0.5 if value == "?" else (1 if key == value else 0) for key in col.has.keys()]
-
-        fa = FactorAnalysis(n_components=2)
-        self._fa_rows = fa.fit_transform(input_)
-
-    def _project(self, row_index: int, a_index: int, b_index: int, c: float):
+        rows = row_cleaning(self._data.rows)
+        best, rest = worker(rows, [])
+        return Data.clone(self._data,best), Data.clone(self._data,rest),11
+    
+    def _project(self, cols: List[Col], row: Row, a: Row, b: Row, c: float):
         return {
-            "row_index": row_index,
+            "row": row,
             "x": cosine_similarity(
-                a=self._distance_class.raw_dist(self._fa_rows[row_index], self._fa_rows[a_index]),
-                b=self._distance_class.raw_dist(self._fa_rows[row_index], self._fa_rows[b_index]),
+                a=self._distance_class.dist(cols, row, a),
+                b=self._distance_class.dist(cols, row, b),
                 c=c
             )
         }
-
-    def _half(self, row_indexes: List[int], above=None):
+    def _half(self, cols: List[Col], rows: List[Row], above=None):
         """
         divides data using 2 far points
         """
-        some = many(row_indexes, self._halves)
+        some = many(rows, self._halves)
 
         a = above if above is not None and self._reuse else any(some)
 
-        tmp = sorted(
-            [
-                {"row_index": i, "d": self._distance_class.raw_dist(self._fa_rows[i], self._fa_rows[a])}
-                for i in some
-            ],
-            key=lambda x: x["d"]
-        )
-
+        tmp = sorted([{"row": r, "d": self._distance_class.dist(cols, r, a)} for r in some], key=lambda x: x["d"])
         far = tmp[int((len(tmp) - 1) * self._far)]
 
-        b, c = far["row_index"], far["d"]
+        b, c = far["row"], far["d"]
 
-        sorted_rows = sorted(
-            map(lambda row_index: self._project(row_index, a, b, c), row_indexes), key=lambda x: x["x"]
-        )
-
+        sorted_rows = sorted(map(lambda row: self._project(cols, row, a, b, c), rows), key=lambda x: x["x"])
         left, right = [], []
 
         for n, two in enumerate(sorted_rows):
-            if (n + 1) <= (len(row_indexes) / 2):
-                left.append(two["row_index"])
+            if (n + 1) <= (len(rows) / 2):
+                left.append(two["row"])
             else:
-                right.append(two["row_index"])
+                right.append(two["row"])
 
         evals = 1 if above is not None and self._reuse else 2
 
         return left, right, a, b, c, evals
 
-    def _sway(self):
-        def worker(row_indexes: List[int], worse, evals0=None, above=None):
-            if len(row_indexes) <= len(self._data.rows) ** self._i_min:
-                return row_indexes, many(worse, self._rest * len(row_indexes)), evals0
+    def _sway(self, cols: List[Col]):
+            def worker(rows: List[Row], worse, evals0=None, above=None):
+                if len(rows) <= len(self._data.rows) ** self._i_min:
+                    return rows, many(worse, self._rest * len(rows)), evals0
 
-            l, r, a, b, c, evals_ = self._half(row_indexes, above)
+                l, r, a, b, c, evals_ = self._half(cols, rows, above)
 
-            if ZitzlerPredicate.better(self._data.cols.y, self._data.rows[b], self._data.rows[a]):
-                l, r, a, b = r, l, b, a
+                if ZitzlerPredicate.better(self._data.cols.y, b, a):
+                    l, r, a, b = r, l, b, a
 
-            for x in r:
-                worse.append(x)
+                for x in r:
+                    worse.append(x)
 
-            return worker(l, worse, evals_ + evals0, a)
+                return worker(l, worse, evals_ + evals0, a)
 
-        best, rest, evals = worker(list(range(len(self._data.rows))), [], 0)
+            best, rest, evals = worker(self._data.rows, [], 0)
 
-        best = [self._data.rows[i] for i in best]
-        rest = [self._data.rows[i] for i in rest]
-
-        return Data.clone(self._data, best), Data.clone(self._data, rest), evals
+            return Data.clone(self._data, best), Data.clone(self._data, rest), evals
+    
+    def better_multiple(self, rows1, rows2, s1=0, s2=0, ys=None, x=0, y=0):
+        if not ys:
+            ys = self._data.cols.y
+        for col in ys:
+            for row1, row2 in zip(rows1, rows2):
+                x = col.normalize(row1.cells[col.at])
+                y = col.normalize(row2.cells[col.at])
+                s1 = s1 - math.exp(col.w * (x - y) / len(ys))
+                s2 = s2 - math.exp(col.w * (y - x) / len(ys))
+        return s1 / len(ys) < s2 / len(ys)
+    
+def row_cleaning(rows):
+    cleaned_rows = []
+    for row in rows:
+        flag = True
+        for cell in row.cells:
+            if isinstance(cell, str) and cell.strip() == '?':
+                flag = False
+                break
+        if flag:
+            cleaned_rows.append(row)
+    return cleaned_rows
