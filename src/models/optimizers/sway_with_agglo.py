3cf82e551f1299d3f79a0407fd64c191c5e10565
j-08-shah <08jainam05shah@gmail.com>
2023-04-18 01:34:48 -0400

    added autoencoded file replacing original file

--- src/models/optimizers/sway_with_agglo.py
+++ src/models/optimizers/sway_with_agglo.py
@@ -30,8 +30,9 @@ class SwayWithAggloOptimizer:
         return self.sway_agglo(self._data.cols.x)
     
     def half_agglo(self, rows=None, cols=None):
+        e = 0
         if not rows:
-            rows = self._data.rowsrows
+            rows = self._data.rows
 
         rows_numpy = np.array([r.cells for r in rows])
         agg = AgglomerativeClustering(n_clusters=2, metric='euclidean', linkage='ward').fit(rows_numpy)
@@ -42,78 +43,26 @@ class SwayWithAggloOptimizer:
                 left.append(rows[idx])
             else:
                 right.append(rows[idx])
-        return left, right, random.choices(left, k=10), random.choices(right, k=10)
+
+        e = 1 if self._reuse else 2
+        return left, right, random.choices(left, k=10), random.choices(right, k=10), e
     
     def sway_agglo(self, cols=None):
         
-        def worker(rows, worse):
+        def worker(rows, worse,evals0):
             if len(rows) <= len(self._data.rows) ** self._i_min:
-                return rows, many(worse, self._rest * len(rows)),
-            l, r, A, B = self.half_agglo(rows, cols)
+                return rows, many(worse, self._rest * len(rows)), evals0
+            l, r, A, B,evals1 = self.half_agglo(rows, cols)
             if self.better_multiple(B, A):
                 l, r, A, B = r, l, B, A
             for x in r:
                 worse.append(x)
-            return worker(l, worse)
+            return worker(l, worse, evals1 + evals0)
 
         rows = row_cleaning(self._data.rows)
-        best, rest = worker(rows, [])
-        return Data.clone(self._data,best), Data.clone(self._data,rest),11
-    
-    def _project(self, cols: List[Col], row: Row, a: Row, b: Row, c: float):
-        return {
-            "row": row,
-            "x": cosine_similarity(
-                a=self._distance_class.dist(cols, row, a),
-                b=self._distance_class.dist(cols, row, b),
-                c=c
-            )
-        }
-    def _half(self, cols: List[Col], rows: List[Row], above=None):
-        """
-        divides data using 2 far points
-        """
-        some = many(rows, self._halves)
+        best, rest, evals = worker(rows, [],0)
+        return Data.clone(self._data,best), Data.clone(self._data,rest),evals
 
-        a = above if above is not None and self._reuse else any(some)
-
-        tmp = sorted([{"row": r, "d": self._distance_class.dist(cols, r, a)} for r in some], key=lambda x: x["d"])
-        far = tmp[int((len(tmp) - 1) * self._far)]
-
-        b, c = far["row"], far["d"]
-
-        sorted_rows = sorted(map(lambda row: self._project(cols, row, a, b, c), rows), key=lambda x: x["x"])
-        left, right = [], []
-
-        for n, two in enumerate(sorted_rows):
-            if (n + 1) <= (len(rows) / 2):
-                left.append(two["row"])
-            else:
-                right.append(two["row"])
-
-        evals = 1 if above is not None and self._reuse else 2
-
-        return left, right, a, b, c, evals
-
-    def _sway(self, cols: List[Col]):
-            def worker(rows: List[Row], worse, evals0=None, above=None):
-                if len(rows) <= len(self._data.rows) ** self._i_min:
-                    return rows, many(worse, self._rest * len(rows)), evals0
-
-                l, r, a, b, c, evals_ = self._half(cols, rows, above)
-
-                if ZitzlerPredicate.better(self._data.cols.y, b, a):
-                    l, r, a, b = r, l, b, a
-
-                for x in r:
-                    worse.append(x)
-
-                return worker(l, worse, evals_ + evals0, a)
-
-            best, rest, evals = worker(self._data.rows, [], 0)
-
-            return Data.clone(self._data, best), Data.clone(self._data, rest), evals
-    
     def better_multiple(self, rows1, rows2, s1=0, s2=0, ys=None, x=0, y=0):
         if not ys:
             ys = self._data.cols.y
